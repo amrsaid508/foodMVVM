5d5de6723e7b8487004f8d4fcd1fb7f7e9534b00
amr <amrsaid508@gmail.com>
2022-12-13 10:53:06 +0200

    initial commit

--- app/src/main/java/com/amroid/foodmvvm/MainActivity.kt
+++ app/src/main/java/com/amroid/foodmvvm/MainActivity.kt
@@ -1,45 +1,13 @@
 package com.amroid.foodmvvm
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import com.amroid.foodmvvm.ui.theme.FoodMVVMTheme
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity(){
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    setContent {
-      Column(modifier = Modifier.padding(16.dp)) {
-        Text(text = "test")
-        Spacer(modifier = Modifier.padding(top = 10.dp))
-        Button(onClick = {
+    setContentView(R.layout.activity_main)
 
-        }) {
-          Text(text = "button")
-        }
-      }
-    }
-  }
-}
-
-@Composable
-fun Greeting(name: String) {
-  Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-  FoodMVVMTheme {
-    Greeting("Android")
   }
 }
